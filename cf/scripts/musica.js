222b823d77d872bb11b8cc1c94d7ae648e032941
davikskk <davimasc33@gmail.com>
2026-08-27 10:42:54 -0300

    concertei o caminho dos sons

--- cf/scripts/musica.js
+++ cf/scripts/musica.js
@@ -1,11 +1,11 @@
 const sons = [
-    new Audio("músicas/nmwl.mp3"),
-    new Audio("músicas/persona.mp3"),
-    new Audio("músicas/heaven.mp3"),
-    new Audio("músicas/harvey.mp3"),
-    new Audio("músicas/sarah.mp3"),
-    new Audio("músicas/ec.mp3"),
-    new Audio("músicas/nytl.mp3")
+    new Audio("cf/músicas/nmwl.mp3"),
+    new Audio("cf/músicas/persona.mp3"),
+    new Audio("cf/músicas/heaven.mp3"),
+    new Audio("cf/músicas/harvey.mp3"),
+    new Audio("cf/músicas/sarah.mp3"),
+    new Audio("cf/músicas/ec.mp3"),
+    new Audio("cf/músicas/nytl.mp3")
 ];
 
 const bot = document.getElementById("butao");
@@ -13,22 +13,29 @@ const bot = document.getElementById("butao");
 let mA = 0;
 let lig = false;
 
-function clicou(){
-  lig = !lig;
-  if(lig){
-    sons[mA].play();
-    bot.classList.add("girando");
-  } else {
-    sons[mA].pause();
-    bot.classList.remove("girando");
-  }
+function clicou() {
+
+    lig = !lig;
+
+    if (lig) {
+        sons[mA].play();
+        bot.classList.add("girando");
+    } else {
+        sons[mA].pause();
+        bot.classList.remove("girando");
+    }
 }
 
 sons.forEach(som => {
-  som.addEventListener("ended", () => {
-    mA = (mA + 1) % sons.length;
-    if(lig){
-      sons[mA].play();
-    }
-  });
+
+    som.addEventListener("ended", () => {
+
+        mA = (mA + 1) % sons.length;
+
+        if (lig) {
+            sons[mA].play();
+        }
+
+    });
+
 });
